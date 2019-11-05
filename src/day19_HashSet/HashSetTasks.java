f231a6926ab13e00118a16bba220300a8982c1cf
TechnoStudy <info@techno.study>
2019-11-04 22:01:31 -0500

    added homework

---
+++ src/day19_HashSet/HashSetTasks.java
@@ -0,0 +1,43 @@
+package day19_HashSet;
+
+public class HashSetTasks {
+//    https://beginnersbook.com/2013/12/hashset-class-in-java-with-example/
+    
+//1. Write a Java program to append the specified element to the end of a hash set.
+//
+//
+//2. Write a Java program to iterate through all elements in a hash list.
+//
+//
+//3. Write a Java program to get the number of elements in a hash set.
+//
+//
+//4. Write a Java program to empty an hash set.
+//
+//
+//5. Write a Java program to test a hash set is empty or not.
+//
+//
+//6. Write a Java program to clone a hash set to another hash set.
+//
+//
+//7. Write a Java program to convert a hash set to an array.
+//
+//
+//8. Write a Java program to convert a hash set to a tree set.
+//
+//
+//9. Write a Java program to convert a hash set to a List/ArrayList.
+//
+//
+//10. Write a Java program to compare two hash set.
+//
+//
+//11. Write a Java program to compare two sets and retain elements which are same on both sets.
+//
+//
+//12. Write a Java program to remove all of the elements from a hash set.
+
+//13. Write a Java Program to find duplications
+    
+}
