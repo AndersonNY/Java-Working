d443096640a3451c667d3d793bd3dc958ce3d4b2
TechnoStudy <info@techno.study>
2019-11-06 14:06:01 -0500

    day 21 added

---
+++ src/day21_Methods/Homework.java
@@ -0,0 +1,11 @@
+package day21_Methods;
+
+public class Homework {
+//    5. Write a Java method to count all words in a string. Go to the editor
+//    Test Data:
+//    Input the string: The quick brown fox jumps over the lazy dog.
+//    Expected Output:
+//
+//    Number of words in the string: 9
+    //advanced, put in a new class and call the method from the class.
+}
